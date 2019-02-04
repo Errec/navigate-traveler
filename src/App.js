28cffd04b5601207bfcbb5da72d89b02b5991388
Raniro Coelho <errecme@gmail.com>
2019-02-03 23:39:34 -0200

    Update dapicker component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import DatePicker from "react-datepicker";
+import { fetchTravels } from "./actions/travelsActions";
+import TravelsList from "./components/TravelsList";
 import { Provider } from "react-redux";
 import store from "./store";
-import TravelsList from "./components/TravelsList";
-import DataSearch from "./components/DataSearch";
 import "./styles/App.sass";
+import "react-datepicker/dist/react-datepicker.css";
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      isLoaded: false,
+      travelData: []
+    };
+ 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date,
+      travelData: [{name: 'joe', number:'3'},{name: 'conan', number:'7'}]
+    });
+
+    this.props.fetchTravels("2019-02-01");
+  }
+
   render() {
-    return (
-      <Provider store={store}>
+    console.log(this.props)
+    return <Provider store={store}>
         <div className="App">
-          <DataSearch></DataSearch>
-          <TravelsList></TravelsList>
-        </div> 
-      </Provider>
-    );
+          <DatePicker
+            minDate={new Date()}
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            dateFormat="dd/MM/yyyy"
+          />
+          <TravelsList travelData={this.state.travelData} />
+        </div>
+      </Provider>;
   }
 }
 
-export default App;
+App.propTypes = {
+  fetchTravels: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.travelsData.travels
+});
+
+export default connect(mapStateToProps, { fetchTravels })(App);
--- src/components/TravelsList.js
+++ src/components/TravelsList.js
@@ -1,35 +1,17 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchTravels } from '../actions/travelsActions';
+import React from 'react'
 
-class TravelsList extends React.Component {
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false
-    };
-  }
-
+export default class TravelsList extends React.Component {
   render() {
-    console.log(this.props.data);
     return (
       <div>
-        <h1> My Boat LIst </h1>
+        <ul>
+          {
+            this.props.travelData.map(travel =>(
+              <li key={travel.name}>{travel.name}</li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
 }
-
-TravelsList.propTypes = {
-  fetchTravels: PropTypes.func.isRequired,
-  data: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  data: state.travelsData.travels
-});
-
-export default connect(mapStateToProps, { fetchTravels })(TravelsList);
